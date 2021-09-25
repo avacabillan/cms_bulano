8fcf24c856095349947e9d40752d313a15140362
Bianca <biancacurtz1999@gmail.com>
2021-09-25 12:09:06 +0800

    Update add_associates.blade.php

--- resources/views/pages/admin/associates/add_associates.blade.php
+++ resources/views/pages/admin/associates/add_associates.blade.php
@@ -19,64 +19,62 @@
                         <h4 class="form-label text-dark">Personal Information</h4> 
                                     <div ><br><br>
                                         <label class="form-label">Name</label> 
-                                        <input type="text" class="form-control" value=""   name="clientname"  >
+                                        <input type="text" class="form-control" value=""   name="assocname"  >
                                         <label class="form-label">Email</label>
-                                        <input type="text" class="form-control" value="">         
+                                        <input type="text" class="form-control" value="" name="assocemail">         
                                         <label class="form-label">Contact Number</label>
-                                        <input type="text" class="form-control" value="" > <br>
+                                        <input type="text" class="form-control" value="" name="asosoccontactno" > <br>
                                         <label class="form-label">SSS Number/Government ID no.</label>
-                                        <input type="text" class="form-control" value="" > <br>
+                                        <input type="text" class="form-control" value="" name="assocsss"> <br>
                                         <label class="form-label">Birth Date</label>
-                                        <input type="date" class="form-control" value="" > <br>
+                                        <input type="date" class="form-control" value="" name="assocbirthdate"> <br>
                                     </div>
                                    
                               
                                     <div>
                                         <label for="inputEmail4" class="form-label">Unit/House No. & Street</label>
-                                        <input type="text" value="" class="form-control" id="inputEmail4">
+                                        <input type="text" value="" class="form-control" id="inputEmail4" name="assocstreet">
                                     </div>
     
                                     <div >
                                         <label for="inputAddress" class="form-label">City/Municipality</label>
-                                        <input type="text" class="form-control" id="inputAddress" value=" ">
+                                        <input type="text" class="form-control" id="inputAddress" value="" name="assoccity">
                                     </div>
                                     <div >
                                         <label for="inputAddress2" class="form-label">Province</label>
-                                        <input type="text" class="form-control" id="inputAddress2" value=" ">
+                                        <input type="text" class="form-control" id="inputAddress2" value="" name="assocprovince">
                                     </div>
                                     <div >
                                         <label for="inputCity" class="form-label">Postal Code</label>
-                                        <input type="text" class="form-control" id="inputCity" value=""><br>
+                                        <input type="text" class="form-control" id="inputCity" value="" name="assocpostal"><br>
                                     </div>
                                 </div>  
                                 <h4 class="form-label text-dark">JOB Information</h4><br><br>
                                     <div >
                                         <label class="form-label">Title</label> 
-                                        <input type="text" class="form-control" value=""   name="clientname"  >
-                                        <label class="form-label">Title</label> 
-                                        <input type="text" class="form-control" value=""   name="clientname"  >
+                                        <input type="text" class="form-control" value=""   name="assoctitle"  >
                                         <label class="form-label">Employee ID</label>
-                                        <input type="text" class="form-control" value="">         
+                                        <input type="text" class="form-control" value="" name="assicemployeeid">         
                                         <label class="form-label">Departmment</label>
-                                        <input type="text" class="form-control" value="" > <br>
+                                        <input type="text" class="form-control" value="" name="assocdepartment" > <br>
                                         <label class="form-label">Supervisor</label>
-                                        <input type="text" class="form-control" value="" > <br>
+                                        <input type="text" class="form-control" value="" name="assocsupervisor"> <br>
                                         <label class="form-label">Work Location</label>
-                                        <input type="date" class="form-control" value="" > <br>
+                                        <input type="date" class="form-control" value="" name="assocworklocation" > <br>
                                         <label class="form-label">Start Date</label>
-                                        <input type="date" class="form-control" value="" > <br>
+                                        <input type="date" class="form-control" value="" name="assocstartdate" > <br>
                                     </div> -->
 
                                 <h4 class="form-label text-dark">Emergency Contact Information</h4> 
                                     <div class="col">
                                         <label class="form-label">Name</label>
-                                        <input type="text" class="form-control me-1" name="regdate"   value=""><br>
+                                        <input type="text" class="form-control me-1" name="regdate"   value="" name="assocemergencyname"><br>
                                         <label class="form-label">Address</label> 
-                                        <input type="text" class="form-control" name="tname"   value=""><br>
+                                        <input type="text" class="form-control" name="tname"   value="" name="assocemergencyaddress"><br>
                                         <label class="form-label">Cell Phone Number</label> 
-                                        <input type="text" class="form-control" name="linebus"   value=""><br>
+                                        <input type="text" class="form-control" name="linebus"   value="" name="assocemergencycontact"><br>
                                         <label class="form-label">Relationship</label> 
-                                        <input type="text" class="form-control" name="linebus"   value=""><br>
+                                        <input type="text" class="form-control" name="linebus"   value="" name="assocemergencyrelationship"><br>
                                     </div> 
 
                         
