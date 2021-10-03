77576c5479ea62085220d6cc7bd42f8664c544cd
Bianca <biancacurtz1999@gmail.com>
2021-10-03 13:12:26 +0800

    update

--- app/Models/AssocAddress.php
+++ app/Models/AssocAddress.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class AssocAddress extends Model
 {
     use HasFactory;
+    protected $table ='assoc_address';
+    
+    protected $fillable = [
+        "house_no_and_street",
+    ];
+
+    public function AssocLocation(){
+        return $this->belongsTo(AssocLocation::class, 'assoc_location_id');
+    }
 }
