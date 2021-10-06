a2f5a4eb6614b9833fb58ce536dad44094f502a7
Bianca <biancacurtz1999@gmail.com>
2021-10-06 22:10:15 +0800

    update

---
+++ public/js/admin_addAssoc.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#btn-addAssoc").click(function(){
+      $(".addAssoc_form").fadeIn(500);
+  }); 
+  $("#close_AssocProfile").click(function(){
+      $(".add_form").fadeOut(500);
+  });
+}); 
