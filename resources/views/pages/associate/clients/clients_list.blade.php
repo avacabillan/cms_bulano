4f9f1fd93832b874367b69ef2ad200191a0952f7
Jean Jati <73151103+jjati@users.noreply.github.com>
2021-10-06 10:15:58 +0800

    update

--- resources/views/pages/associate/clients/clients_list.blade.php
+++ resources/views/pages/associate/clients/clients_list.blade.php
@@ -40,21 +40,18 @@
  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
-            <div class="modal-content">
+            <div class="modal-content" style="width: 120%;">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">My Profile</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <button class="btn btn-outline-success btn-sm mt-3 mb-2" style="float: right; width:30%;"><i class="fas fa-plus-circle"></i> Add New Folder</button>
+                <!-- <button class="btn btn-outline-success btn-sm mt-3 mb-2" style="float: right; width:30%;"><i class="fas fa-plus-circle"></i> Add New Folder</button> -->
                 <div class="modal-body">
                 @include('pages.associate.clients.client_profile')
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+              
             </div>
         </div>
     </div>
