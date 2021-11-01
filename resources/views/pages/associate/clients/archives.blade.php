4b5efafb658ca5bd17d600c3cc6d6c0fc723cb63
Jean Jati <73151103+jjati@users.noreply.github.com>
2021-11-01 13:18:50 +0800

    update

--- resources/views/pages/associate/clients/archives.blade.php
+++ resources/views/pages/associate/clients/archives.blade.php
@@ -5,8 +5,8 @@
 @section('content')
 <div class="col-md-8 offset-md-2 mt-5">
 
-<table class="table table-light table-striped mt-3">
-    <thead>
+    <table class="table table-light table-striped mt-5">
+        <thead>
             <tr>
                 <th  class="text-center text-dark" scope="col">File name</th>
                 <th  class="text-center text-dark" scope="col">File type</th>
@@ -14,26 +14,26 @@
                 <th  class="text-center text-dark" scope="col">Delete_at</th>
                 <th  class="text-center text-dark" scope="col">Restore</th>
             </tr>
-    </thead>
-    <tbody>
-    @foreach($onlySoftDeleted as $onlySoftDeleteds)
-        <tr class="text-center" >
-            <td>{{  $onlySoftDeleteds->file_name }}</td>
-            <td>{{  $onlySoftDeleteds->file_type }}</td>
-            <td>{{ $onlySoftDeleteds->description }}</td>
+        </thead>
+        <tbody>
+        @foreach($onlySoftDeleted as $onlySoftDeleteds)
+            <tr class="text-center" >
+                <td>{{  $onlySoftDeleteds->file_name }}</td>
+                <td>{{  $onlySoftDeleteds->file_type }}</td>
+                <td>{{ $onlySoftDeleteds->description }}</td>
            
-            <td>{{ date_format($onlySoftDeleteds->deleted_at, 'jS M Y') }}</td>
-            <td>
+                <td>{{ date_format($onlySoftDeleteds->deleted_at, 'jS M Y') }}</td>
+                <td>
                 
                    <a class="text-dark" href="{{ route('restore', $onlySoftDeleteds->id) }}" title="restore project"> Restore</a>
                 
-            </td>
-        </tr>
-        <tr>
+                </td>
+            </tr>
+            <tr>
             
-        </tr>
-    @endforeach
-    </tbody>
-</table>  
+            </tr>
+        @endforeach
+        </tbody>
+    </table>  
 </div> 
 @endsection
