63544ae7da889a19df195cd5c545693c89dc5b70
Jean Jati <73151103+jjati@users.noreply.github.com>
2022-01-20 22:45:35 +0800

    update

--- resources/views/pages/admin/messages/admin_message.blade.php
+++ resources/views/pages/admin/messages/admin_message.blade.php
@@ -41,7 +41,7 @@
       </div>
 
       <div class="modal-body" class="message_box">
-        <form id="Form" action="{{route('admin_showmsg',id)}}" method="post">
+        <form id="Form" action="{{route('admin_showmsg',1)}}" method="post">
           @csrf
           @method('post')
           <div class="input-group doctor-compose">
