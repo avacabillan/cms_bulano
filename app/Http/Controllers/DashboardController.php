fedbd8d2eafa952d19ca6878435f821663701195
avacabillan <73097784+avacabillan@users.noreply.github.com>
2022-01-26 17:22:18 -0800

    auth remove

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,35 +9,36 @@ use DB;
 class DashboardController extends Controller
 {
     public function index(){
-        if(Auth::user()->hasRole('admin')){ 
-            $associates = Associate::all();
+        // if(Auth::user()->hasRole('admin')){ 
+        //     $associates = Associate::all();
              
             
 
             
 
-            // dd($associates); 
+        //     // dd($associates); 
           
-            // $assocs_1 = Associate::find(1);
-            // $assocs_2 = Associate::find(2);
-            // $assocs_3 = Associate::find(3);
-            // $assocClient1 = DB::table('clients')
-            // ->where('assoc_id', '=', 1)
-            // ->count();
-            // $assocClient2 = DB::table('clients')
-            // ->where('assoc_id', '=', 2)
-            // ->count();
-            // $assocClient3 = DB::table('clients')
-            // ->where('assoc_id', '=', 3)
-            // ->count();   
-            return view ('pages.admin.dashboard', compact('associates'));
+        //     // $assocs_1 = Associate::find(1);
+        //     // $assocs_2 = Associate::find(2);
+        //     // $assocs_3 = Associate::find(3);
+        //     // $assocClient1 = DB::table('clients')
+        //     // ->where('assoc_id', '=', 1)
+        //     // ->count();
+        //     // $assocClient2 = DB::table('clients')
+        //     // ->where('assoc_id', '=', 2)
+        //     // ->count();
+        //     // $assocClient3 = DB::table('clients')
+        //     // ->where('assoc_id', '=', 3)
+        //     // ->count();   
+        //     return view ('pages.admin.dashboard', compact('associates'));
 
 
-        }elseif (Auth::user()->hasRole('associate')){
-            return view ('pages.associate.dashboard');
-        }elseif (Auth::user()->hasRole('client')){
-            return view ('pages.client.dashboard');
-        }
+        // }elseif (Auth::user()->hasRole('associate')){
+        //     return view ('pages.associate.dashboard');
+        // }elseif (Auth::user()->hasRole('client')){
+        //     return view ('pages.client.dashboard');
+        // }
+      
     }
     public function countClient(){
         $clients= DB::table('users')
--- resources/views/pages/admin/sidebar.blade.php
+++ resources/views/pages/admin/sidebar.blade.php
@@ -12,8 +12,7 @@
             <img class="img-responsive img-rounded" src="images/bulano.png" alt="User picture">
           </div>
           <div class="user-info">
-            <span class="user-name text-white">{{Auth::user()->name}}</span>
-            <span class="user-role">{{Auth::user()->roles()->first()->display_name}}</span>
+            
           </div>
         </div>
         <!-- END OF SIDEBAR HEADER -->
@@ -27,7 +26,7 @@
             
             <!-- DASHBOARD -->
             <li>
-              <a href="{{route('dashboard')}}"><i class="fa fa-book position-relative"></i>
+              <a href=""><i class="fa fa-book position-relative"></i>
                 <span>Dashboard</span>
               </a>
             </li>
